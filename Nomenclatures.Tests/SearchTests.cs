2b2e5f12449fa0e39b0ac90da91299cb8a955338
Olivier Le Guyader <olivier.leguyader@alcuin.com>
2021-10-21 17:23:55 +0200

    Recherche

---
+++ Nomenclatures.Tests/SearchTests.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Nomenclatures.Tests
+{
+    public class SearchTests
+    {
+        private List<Nomenclatures.Data.FamilleMatierePremiere> _familles;
+        private List<Nomenclatures.Data.MatierePremiere> _matieresPremieres;
+        private List<Nomenclatures.Data.Produit> _produits;
+
+        [Test]
+        public void SimpleSearch()
+        {
+            var search = new Search(
+                _familles.AsQueryable(), 
+                _matieresPremieres.AsQueryable(),
+                _produits.AsQueryable())
+            {
+                NomContient = "i"
+            };
+
+            var result = search.Execute();
+
+            Assert.AreEqual(6, result.Count());
+            Assert.IsFalse(result.Any(r => r.Nom == "Chocolat"));
+        }
+
+        [Test]
+        public void SearchForBio()
+        {
+            var search = new Search(
+                _familles.AsQueryable(), 
+                _matieresPremieres.AsQueryable(),
+                _produits.AsQueryable())
+            {
+                EstBio = true
+            };
+
+            var result = search.Execute();
+
+            Assert.AreEqual(3, result.Count());
+            Assert.IsFalse(result.Any(r => r.Nom == "famille farine"));
+        }
+
+        [Test]
+        public void SearchByType()
+        {
+            var search = new Search(
+                _familles.AsQueryable(), 
+                _matieresPremieres.AsQueryable(),
+                _produits.AsQueryable())
+            {
+                TypesRecherches = new Type[] { typeof(Data.MatierePremiere), typeof(Data.ProduitFini) }
+            };
+
+            var result = search.Execute();
+
+            Assert.AreEqual(4, result.Count());
+            Assert.IsFalse(result.Any(r => r.Nom == "famille farine"));
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _familles = new List<Nomenclatures.Data.FamilleMatierePremiere>()
+            {
+                new Data.FamilleMatierePremiere() { Id = 1, Nom = "famille farine" },
+                new Data.FamilleMatierePremiere() { Id = 2, Nom = "famille arôme" }
+            };
+
+            _matieresPremieres = new List<Nomenclatures.Data.MatierePremiere>()
+            {
+                new Data.MatierePremiere() { Id = 1, Nom = "Farine bio", Bio = true},
+                new Data.MatierePremiere() { Id = 2, Nom = "Farine", Bio = false},
+                new Data.MatierePremiere() { Id = 3, Nom = "Chocolat", Bio = false},
+            };
+
+            _produits = new List<Nomenclatures.Data.Produit>()
+            {
+                new Data.ProduitFini { Id = 1, Nom = "Paquet de pitchs", Bio = true },
+                new Data.ProduitSemiFini { Id = 2, Nom = "Pitch", Bio = true }
+            };
+        }
+    }
+}
---
+++ Nomenclatures/Search.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nomenclatures
+{
+    public class Search
+    {
+        private IQueryable<Nomenclatures.Data.FamilleMatierePremiere> _familles;
+        private IQueryable<Nomenclatures.Data.MatierePremiere> _matieres;
+        private IQueryable<Nomenclatures.Data.Produit> _produits;
+
+        public Search(IQueryable<Nomenclatures.Data.FamilleMatierePremiere> familles,
+            IQueryable<Nomenclatures.Data.MatierePremiere> matieres,
+            IQueryable<Nomenclatures.Data.Produit> produits)
+        {
+            _familles = familles;
+            _matieres = matieres;
+            _produits = produits;
+        }
+
+        public string NomContient { get; set; }
+
+        public bool? EstBio { get; set; }
+
+        public Type[] TypesRecherches { get; set; }
+
+        public IEnumerable<SearchResult> Execute()
+        {
+            var query = FiltrerMatieres()
+                    .Select(mp => new SearchResult { Id = mp.Id, Nom = mp.Nom, Type = nameof(MatierePremiere) })
+                .Union(FiltrerProduits()
+                    .Select(p => new SearchResult { Id = p.Id, Nom = p.Nom, Type = nameof(Produit) }));
+            
+            if(!EstBio.HasValue)
+                query = query.Union(FiltrerFamilles()
+                        .Select(f => new SearchResult { Id = f.Id, Nom = f.Nom, Type = nameof(FamilleMatierePremiere) }));
+
+            return query;
+        }
+
+        private IQueryable<Nomenclatures.Data.FamilleMatierePremiere> FiltrerFamilles()
+        {
+            var query = _familles;
+            
+            if(!string.IsNullOrEmpty(NomContient))
+                query = query.Where(f => f.Nom.Contains(NomContient));
+
+            return query;
+        }
+
+        private IQueryable<Nomenclatures.Data.MatierePremiere> FiltrerMatieres()
+        {
+            var query = _matieres;
+
+            if(!string.IsNullOrEmpty(NomContient))
+                query = query.Where(m => m.Nom.Contains(NomContient));
+
+            if(EstBio.HasValue)
+                query = query.Where(m => m.Bio == EstBio.Value);
+
+            return query;
+        }
+
+        private IQueryable<Nomenclatures.Data.Produit> FiltrerProduits()
+        {
+            var query = _produits;
+
+            if(!string.IsNullOrEmpty(NomContient))
+                query = query.Where(p => p.Nom.Contains(NomContient));
+
+            if(EstBio.HasValue)
+                query = query.Where(p => p.Bio == EstBio.Value);
+
+            return query;
+        }
+    }
+
+    public class SearchResult
+    {
+        public int Id { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Type { get; set; }
+    }
+}
